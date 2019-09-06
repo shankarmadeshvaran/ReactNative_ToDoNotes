839df39fbda70381a3f5e9b4502e57ba0a4c1b72
shankarmadeshvaran <shankarmadeshvaran@gmail.com>
2019-09-06 10:16:33 +0530

    First React-Native Project ToDo Notes

---
+++ ToDoNotes/App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import Main from './ios/app/components/Main'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
